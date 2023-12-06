6662b383d69e1e83fb6b7875a21ede4cbaa19c52
[rugren] <[rubgarciaruiz@gmail.com]>
2023-12-06 22:51:34 +0100

    Corregidos ejercicios.
    Y en index.php de Ejer03DadosPoker.
    El creador de este ejercicio le faltaba poner comillas simples ' ' a misDados y tiradasTotales, así que no los cogía bien. Corregido yo el 06/12/2023.
    
     EN TODOS LOS EJERCICIOS QUE FALLEN COSAS ASÍ SEGURO QUE ES POR LAS COMILLAS SIMPLES, que no se las habrá puesto.

--- PHP0/index.php
+++ PHP0/index.php
@@ -2,15 +2,14 @@
     <head>
     <meta charset="UTF-8">
         <title>Esta es mi web</title>
-    <head>
+    </head>
         
     <body>
         <?php
-        /*echo "Hola Mundo"
+        /*echo "Hola Mundo"; */
 
         $variable = 7;
         echo $variable;
-        */
     ?>
     </body>
 </html>
@@ -19,23 +18,23 @@
 <html>
     <head>
         <meta charset="UTF-8">
-</head> 
+    </head> 
     <body>
         <?php
        $a = 7;
        $b = 9;
-       echo "<h2>" $a + $b, "<br>";
+       echo "<h2>", $a + $b, "<br>";
        echo $a - $b, "<br>";
        echo $a * $b, "<br>";
        echo $a / $b, "<br>";
 
        $a++;
-       echo $a "<br>";
+       echo $a, "<br>";
 
        $b++;
-       echo $b "<br>";
+       echo $b, "<br>";
 
-       echo "<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png">;
+       echo '<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/2048px-Visual_Studio_Code_1.35_icon.svg.png">';
     ?>
     </body>
 </html>
