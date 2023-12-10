3599372f3dff4e34d43bd0df77f5cf65c76db0cd
[rugren] <[rubgarciaruiz@gmail.com]>
2023-12-10 23:40:50 +0100

    Editados ejercicios examen prueba EntornoServidor y añadidas mejoras (Aún por funcionar correctamente)

--- EntornoServidor/EntornoServidor_Examen1EVAL/E2/E2.php
+++ EntornoServidor/EntornoServidor_Examen1EVAL/E2/E2.php
@@ -4,22 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulario de Resultados</title>
+
+    <!-- Metido bootstrap para hacerlo más vistoso -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
     <style>
         /* Estilos para ocultar los campos de texto después de escribir */
-        input:placeholder-shown {
+        /* input:placeholder-shown {
             visibility: visible;
         }
 
         input::placeholder {
             visibility: hidden;
-        }
+        } */
     </style>
 </head>
 <body>
 
     <h2>Formulario de Resultados</h2>
 
-    <form id="resultForm">
+    <!-- Así creado normal:
+        <form id="resultForm">
         <label for="team1Name">Nombre equipo 1:</label>
         <input type="text" id="team1Name" placeholder="Nombre equipo 1">
 
@@ -32,7 +37,29 @@
         <label for="team2Points">Puntos equipo 2:</label>
         <input type="number" id="team2Points" placeholder="Puntos equipo 2">
 
-        <button type="button" onclick="calcularGanador()">Calcula el ganador</button>
+        <button type="button" onclick="calcularGanador()">Calcular Ganador</button>
+    </form> -->
+
+    <!-- Así creado como lo pide el profesor en el ejercicio, lo más parecido posible -->
+    <!-- Solo quiere el Nombre equipo 1 dentro del campo vacío y que se elimine conforme escribes -->
+    <form id="resultForm">
+        <label for="team1Name"></label>
+        <input type="text" id="team1Name" placeholder="Nombre equipo 1">
+        <br>
+        <br>
+        <label for="team1Points"></label>
+        <input type="number" id="team1Points" placeholder="Puntos del equipo 1">
+        <br>
+        <br>
+        <label for="team2Name"></label>
+        <input type="text" id="team2Name" placeholder="Nombre equipo 2">
+        <br>
+        <br>
+        <label for="team2Points"></label>
+        <input type="number" id="team2Points" placeholder="Puntos del equipo 2">
+        <br>
+        <br>
+        <button type="button" onclick="calcularGanador()" class="btn btn-primary" >Submit</button>
     </form>
 
     <div id="resultMessage" style="display: none;">
@@ -60,6 +87,9 @@
             resultMessage.style.display = 'block';
         }
     </script>
+
+<!-- El script de bootstrap:  -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 </html>
--- EntornoServidor/EntornoServidor_Examen1EVAL/E3/E3.php
+++ EntornoServidor/EntornoServidor_Examen1EVAL/E3/E3.php
@@ -4,10 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Partido Aleatorio</title>
+    
+    <!-- Metido bootstrap para hacerlo más vistoso -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
 </head>
 <body>
 
- <!-- la función rand de PHP para seleccionar de manera aleatoria dos equipos de un array
+<!-- la función rand de PHP para seleccionar de manera aleatoria dos equipos de un array
 En este ejemplo, el array $equipos contiene los nombres de los equipos. 
 Se utilizan las funciones rand y count para seleccionar aleatoriamente dos equipos del array. 
 Además, se incluye una verificación para asegurarse de que los dos equipos seleccionados no sean iguales. 
@@ -22,7 +26,7 @@ $equipos = ["Equipo A", "Equipo B", "Equipo C", "Equipo D", "Equipo E"];
 $equipo1 = $equipos[rand(0, count($equipos) - 1)];
 $equipo2 = $equipos[rand(0, count($equipos) - 1)];
 
-// Asegurarse de que los equipos seleccionados no sean iguales
+// Esto es para asegurarse de que los equipos seleccionados no sean iguales
 while ($equipo1 === $equipo2) {
     $equipo2 = $equipos[rand(0, count($equipos) - 1)];
 }
@@ -32,6 +36,9 @@ echo "<h2>Partido Aleatorio</h2>";
 echo "<p>Equipo 1: $equipo1</p>";
 echo "<p>Equipo 2: $equipo2</p>";
 ?>
+
+<!-- El script de bootstrap:  -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 </html>
